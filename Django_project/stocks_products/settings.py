c266e804b1633f483c7fe8301e4793fc9b4e0b0d
Maxim Vasilkin <mxvas@mail.ru>
2023-03-26 16:39:37 +0400

    add postgres and nginx

--- Django_project/stocks_products/settings.py
+++ Django_project/stocks_products/settings.py
@@ -80,9 +80,13 @@ WSGI_APPLICATION = 'stocks_products.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD')
+   }
 }
 
 
